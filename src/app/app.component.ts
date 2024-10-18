113b0a60a53cd4551f74e333e80d6b45b899b438
DESKTOP Andre <andrebutze.mail@googlemail.com>
2024-10-18 17:43:25 +0200

    AB: Making HttpClient working, only without service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
+
+import { AccountService } from './services/account.service';
+import { IAccount } from './models/Account';
+
+var ACCOUNTS: IAccount[]
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HttpClientModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'FinanceManagerFrontend';
+  accounts = ACCOUNTS
+
+  // constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) {}
+
+  ngAfterViewInit(): void {
+    // this.getAccounts()
+  }
+
+  getAccounts(): void {
+    this.httpClient.get<IAccount[]>('https://localhost:7128/api/Accounts').subscribe((data) => {
+      this.accounts = data
+      const jsonString: string = JSON.stringify(this.accounts)
+      console.log(jsonString)
+    })
+
+    
+  }
 }
--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -1,12 +1,27 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClientModule, HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
+
+import { IAccount } from '../models/Account';
+
+var ACCOUNTS: IAccount[]
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
+  accounts = ACCOUNTS
 
   constructor() { }
+
+  // public getAccounts(): Observable<IAccount[]> {
+  public getAccounts() {
+    // this.httpClient.get<IAccount[]>('https://localhost:7128/api/Accounts').subscribe((data) => {
+    //   console.log(data)
+    //   // this.accounts = data
+    // })
+
+    // return this.accounts
+  }
 }
